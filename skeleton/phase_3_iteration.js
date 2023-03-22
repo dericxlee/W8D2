66db250e4d8b56361ff2cafc1e7a5da4fc7380cb
AA Student <appacademy@Mini-46.local>
2023-03-21 17:37:44 -0700

    stuff

--- skeleton/phase_3_iteration.js
+++ skeleton/phase_3_iteration.js
@@ -1,23 +1,32 @@
 Array.prototype.bubbleSort = function() {
     let sorted = false
-    // console.log(sorted, "first")
     while(sorted === false){
-       console.log(sorted, 'second')
-        for(let i = 0; i < this.length-1; i++){
+        sorted = true
+        for(let i = 0; i < this.length; i++){
             el = this[i];
             secEl = this[i+1];
             if (el > secEl){
                 this[i] = secEl;
                 this[i+1] = el;
                 sorted = false;
-            } else {
-                sorted = true;
-            }
+            } 
         }
     }
-   console.log(sorted, 'third')
     return this;
 }
 
 arr = [5, 7, 3, 1, 6]
 console.log(arr.bubbleSort())
+
+String.prototype.substrings = function(){
+    let newArr = []
+    for(i = 0; i < this.length-1; i += 1){
+        for(j = i+1; j < this.length; j += 1){
+            newArr.push(this.slice(i, j))
+        }
+    }
+    return newArr;
+}
+
+let str = 'abcd'
+console.log(str.substrings());
